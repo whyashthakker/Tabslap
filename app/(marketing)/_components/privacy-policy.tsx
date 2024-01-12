56abba46c8e69fcc2a33f5ce75996d4cd47ea404
whyashthakker <nynansnh@gmail.com>
2024-01-12 13:12:44 +0530

    updated privacy

--- app/(marketing)/_components/privacy-policy.tsx
+++ app/(marketing)/_components/privacy-policy.tsx
@@ -7,7 +7,7 @@ const PrivacyPolicy: React.FC = () => {
         <div className="flex justify-center items-center bg-white p-4">
             <div className="max-w-6xl bg-white p-6 rounded-lg">
                 <h1 className="text-xl font-semibold text-center mb-4">Privacy Policy</h1>
-                <p className="text-sm text-gray-600 mb-4">Last updated: {todayDate}</p>
+                <p className="text-sm text-gray-600 mb-4">Last updated: 11th January, 2024.</p>
 
                 <h2 className="text-lg font-semibold mb-2">1. Introduction</h2>
                 <p>Olly, your personal AI social media assistant, is committed to protecting your privacy. This Privacy Policy outlines our practices regarding the handling of information in relation to the Olly extension, including what information we do not collect, how we use data, and your rights concerning privacy.</p>
@@ -25,7 +25,7 @@ const PrivacyPolicy: React.FC = () => {
                 <p>Olly is dedicated to complying with relevant privacy laws and regulations. Our practices are designed to uphold legal standards and respect your privacy rights.</p>
 
                 <h2 className="text-lg font-semibold mb-2 mt-4">6. Policy Updates</h2>
-                <p>Our Privacy Policy may be updated to reflect changes in our practices or legal obligations. We will inform users of any significant changes by updating the [Last updated] date at the top of this policy. Regular review of this policy is recommended.</p>
+                <p>Our Privacy Policy may be updated to reflect changes in our practices or legal obligations. We will inform users of any significant changes by updating the &#8220;Last Updated Date&#8221; at the top of this policy. Regular review of this policy is recommended.</p>
 
                 <h2 className="text-lg font-semibold mb-2 mt-4">7. Contact Us</h2>
                 <p>For any questions or concerns about our Privacy Policy or practices, please reach out to us at yash@yashthakker.com.</p>
