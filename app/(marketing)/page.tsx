f9e89b4d21df79007a51e48fc3f9d9a407e1e692
whyashthakker <nynansnh@gmail.com>
2024-01-31 19:23:34 +0530

    1090px

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -24,7 +24,7 @@ import UserReviews from "./_components/user-reviews";
 
 const MarketingPage = () => {
   return (
-<div className="min-h-full flex flex-col bg-no-repeat bg-[url('/sm_website_bg.jpg')] lg:bg-[url('/lg_website_bg.jpg')] md:bg-[url('/md_website_bg.jpg')] ">
+<div className="min-h-full flex flex-col bg-no-repeat bg-[url('/sm_website_bg.jpg')] lg:bg-[url('/lg_website_bg.jpg')] md:bg-[url('/md_website_bg.jpg')] xl:bg-[url('/xl_website_bg.png')]">
       {/* <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-1 flex-1 px-4 md:px-6 pb-10"> */}
         <Heading />
         {/* <SocialProof /> */}
