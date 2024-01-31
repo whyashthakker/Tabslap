2a178fb836c72bd9b6501ba1a4fc2838972bfc9b
whyashthakker <nynansnh@gmail.com>
2024-01-31 19:46:39 +0530

    posthog removed shadow added

--- app/(marketing)/_components/cta-buttons.tsx
+++ app/(marketing)/_components/cta-buttons.tsx
@@ -2,19 +2,19 @@
 
 import { ChromeIcon, GithubIcon, Heart } from "lucide-react";
 import { Button } from "@/components/Button";
-import { usePostHog } from "posthog-js/react";
+// import { usePostHog } from "posthog-js/react";
 
 export function CTAButtons() {
-  const posthog = usePostHog();
+//   const posthog = usePostHog();
 
   return (
     <div className="mt-10 grid items-center justify-center gap-x-6 gap-y-2 sm:flex">
       <Button
         size="2xl"
         link={{ href: "https://chromewebstore.google.com/detail/olly-social-media-sidekic/ofjpapfmglfjdhmadpegoeifocomaeje", target: "_blank"}}
-        onClick={() => {
-          posthog.capture("Clicked Get Started");
-        }}
+        // onClick={() => {
+        //   posthog.capture("Clicked Get Started");
+        // }}
       >
         <ChromeIcon className="mr-2 h-4 w-4" />
         Add Olly to Chrome
@@ -23,9 +23,10 @@ export function CTAButtons() {
         size="2xl"
         color="white"
         link={{ href: "https://safegpt.lemonsqueezy.com/checkout/buy/ccbea37a-e676-45b7-8052-e2f45c0210ca", target: "_blank" }}
-        onClick={() => {
-          posthog.capture("Clicked Star on Github", {});
-        }}
+        // onClick={() => {
+        //   posthog.capture("Clicked Star on Github", {});
+        // }}
+        className="shadow-lg shadow-teal-500/50"
       >
         <Heart className="mr-2 h-4 w-4 fill-red-500" />
         Get Lifetime Access
