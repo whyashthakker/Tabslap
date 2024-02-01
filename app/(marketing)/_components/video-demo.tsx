8fcefd8bd0c1312f6b95c74e87b60d9520e667e3
whyashthakker <nynansnh@gmail.com>
2024-02-01 15:27:25 +0530

    modal fix

--- app/(marketing)/_components/video-demo.tsx
+++ app/(marketing)/_components/video-demo.tsx
@@ -12,10 +12,10 @@ export function VideoDemo() {
     <>
       <div
         className="group absolute inset-0 flex cursor-pointer items-center justify-center duration-500 hover:bg-white hover:bg-opacity-30 lg:rounded-2xl shadow-lg shadow-teal-500/50"
-        // onClick={() => {
-        //   openModal();
-        //   posthog.capture("Landing Page Video Clicked");
-        // }}
+        onClick={() => {
+          openModal();
+          // posthog.capture("Landing Page Video Clicked");
+        }}
       >
         <div className="inline-flex h-16 w-16 animate-pulse items-center justify-center rounded-full bg-blue-400 text-slate-50 shadow-lg transition-transform focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 group-hover:scale-125 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90 dark:focus-visible:ring-slate-300">
           <PlayIcon className="h-7 w-7" />
@@ -25,7 +25,7 @@ export function VideoDemo() {
         isOpen={isModalOpen}
         hideModal={closeModal}
         padding="none"
-        size="4xl"
+        size="6xl"
         backdropClass="backdrop-blur bg-white/80"
       >
         <iframe
