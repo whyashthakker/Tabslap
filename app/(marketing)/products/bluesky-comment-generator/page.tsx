80b6c4004e0a84f184a28193a94fae63afe03731
whyashthakker <nynansnh@gmail.com>
2024-02-07 14:42:33 +0530

    added bluesky support

---
+++ app/(marketing)/products/bluesky-comment-generator/page.tsx
@@ -0,0 +1,37 @@
+import { Suspense } from "react";
+import { Metadata } from "next";
+// import { LogoCloud } from "@/app/(landing)/home/LogoCloud";
+
+import { Heading } from "../../_components/heading";
+import { Testimonials } from "../../_components/testimonials";
+import { FeaturesAutomation } from "../../_components/features";
+import { Pricing } from "../../_components/pricing-2";
+import FAQs from "../../_components/faq-section";
+import { CTA } from "../../_components/cta";
+
+export const metadata: Metadata = {
+  title: "Bluesky Comment Generator Online | Olly.social",
+  description:
+    "Olly, your AI-Powered Social Media Assistant to Generated Comments, Posts, Viral Scores for Your Bluesky social media posts. Try Olly's AI Comment Generator for free. Answer things like: What is Bluesky? How to use Bluesky? Bluesky AI and so on.",
+  alternates: { canonical: "/bluesky-comment-generator" },
+};
+
+export default function AICommentGeneratorFree() {
+  return (
+    <>
+      <Heading
+        title="Bluesky Comment Generator"
+        subtitle="Olly, your AI-Powered Social Media Assistant to Generated Comments, Posts, Viral Scores for Your Bluesky social media posts. Try Olly's AI Comment Generator for free."
+        image="/step_main.gif"
+      />
+      {/* <LogoCloud /> */}
+      <Testimonials />
+      <FeaturesAutomation />
+      <Suspense>
+        <Pricing />
+      </Suspense>
+      <FAQs />
+      <CTA />
+    </>
+  );
+}
