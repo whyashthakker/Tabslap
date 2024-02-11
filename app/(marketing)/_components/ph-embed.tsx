573cd4c4ef01d509d13237768f52d236107da9bf
whyashthakker <nynansnh@gmail.com>
2024-02-11 10:30:57 +0530

    ph color neutral

--- app/(marketing)/_components/ph-embed.tsx
+++ app/(marketing)/_components/ph-embed.tsx
@@ -6,7 +6,7 @@ const ProductHuntBadge = () => {
     <div className="flex flex-col items-center justify-center gap-2 sm:flex-row">
       <a href="https://www.producthunt.com/posts/olly-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-olly-2" target="_blank" rel="noopener noreferrer">
         <Image 
-          src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=430993&theme=light&period=daily" 
+          src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=430993&theme=neutral&period=daily" 
           alt="Olly - In-browser AI comments, posts, viral scores in seconds | Product Hunt" 
           width={200} 
           height={30} 
