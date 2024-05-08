e0f7c08584ed950fbe8589dfa871b83e52a352d5
whyashthakker <nynansnh@gmail.com>
2024-05-08 16:13:27 +0530

    updated

--- data/post-data.ts
+++ data/post-data.ts
@@ -1,7 +1,7 @@
 const postData = [
   {
     "title": "Why Commenting is the Biggest Hack on Social Media",
-    "file": "4-use-cases-for-generative-ai-in-data-analytics",
+    "file": "why-commenting-is-the-biggest-hack-on-social-media",
     "description": "Discover the power of commenting on social media and how it can boost your engagement, reach, and influence. Learn the best practices and strategies for effective commenting.",
     "date": "May 8, 2024",
     "datetime": "2024-05-08",
